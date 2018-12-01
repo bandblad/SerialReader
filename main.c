40b75a5dcdf1ca9d307fe635a47fefc82ed80346
Vladislav Akimov <vlad.bandblad@gmail.com>
2018-12-02 00:28:51 +0400

    Small cup of refactoring

--- main.c
+++ main.c
@@ -12,7 +12,11 @@ int main(int argc, char **argv) {
 
     // Get command line arguments
     if (argc != 2) {
+        // Print error message
         printf("Invalid command line arguments!");
+
+        // Quit program
+        return 0;
     }
     else {
         // Save name of serial port
@@ -27,53 +31,58 @@ int main(int argc, char **argv) {
     if (serial == -1) {
         // Print error message
         printf("Failed to open serial port!");
-    } else {
-        // Set Serial Port speed
-        struct termios settings;
 
-        // Get current settings of Serial Port
-        if (tcgetattr(serial, &settings)) {
-            // Quit if error occurred
-            printf("Failed to get serial port settings!");
-            return 0;
+        // Quit program
+        return 0;
+    }
+
+    // Set Serial Port speed
+    struct termios settings;
+
+    // Get current settings of Serial Port
+    if (tcgetattr(serial, &settings)) {
+        // Print error message
+        printf("Failed to get serial port settings!");
+
+        // Quit program
+        return 0;
+    }
+
+    // Disable parity flag
+    settings.c_cflag &= ~(CSIZE | PARENB);
+
+    // Force 8-bit input
+    settings.c_cflag |= CS8;
+
+    // Set RX/TX speed to 57600
+    cfsetspeed(&settings, B57600);
+
+    // Save changes
+    tcsetattr(serial, TCSANOW, &settings);
+
+    // Allocate buffer for
+    // incoming data
+    float buffer;
+
+    // Begin endless read() loop
+    for (long w;;) {
+        // Receive bytes from Serial Port
+        w = read(serial, &buffer, sizeof(float));
+
+        // Check for errors
+        if (w >= 0) {
+            // Print value
+            printf("Received double: %lf\n", buffer);
+        } else {
+            // Print error message
+            printf("Failed to read from serial port!");
+            break;
         }
+    }
 
-        // Disable parity flag
-        settings.c_cflag &= ~(CSIZE | PARENB);
-
-        // Force 8-bit input
-        settings.c_cflag |= CS8;
-
-        // Set RX/TX speed to 57600
-        cfsetspeed(&settings, B57600);
-
-        // Save changes
-        tcsetattr(serial, TCSANOW, &settings);
-
-        // Allocate buffer for
-        // incoming data
-        float buffer;
-
-        // Begin endless read() loop
-        for (long w;;) {
-            // Receive bytes from Serial Port
-            w = read(serial, &buffer, sizeof(float));
-
-            // Check for errors
-            if (w >= 0) {
-                // Print value
-                printf("Received double: %lf\n", buffer);
-            } else {
-                // Print error message
-                printf("Failed to read from serial port!");
-                break;
-            }
-        }
-
-        // Close Serial Port
-        if (close(serial)) {
-            printf("Failed to close serial port!");
-        }
+    // Close Serial Port
+    if (close(serial)) {
+        printf("Failed to close serial port!");
     }
 
     return 0;
